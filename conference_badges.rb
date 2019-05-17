e211473540b3f8e8a739b695f907ef069bce2ae0
Rohit Pratti <rohit.pratti@gmail.com>
2019-05-17 00:23:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker(whoDis)
+  
+  return "Hello, my name is #{whoDis}."
+
+end
+
+def batch_badge_creator(speakinBouls)
+
+badge_plug = []
+
+
+
+
+end
+
+
+def assign_rooms
+
+end
+
+def printer
+
+end
